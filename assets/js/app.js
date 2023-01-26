84892cf63ac25d45b12989bddd7913d2983819e4
jorge <jorgesepdoc@gmail.com>
2023-01-26 02:51:56 -0300

    hola

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,92 +2,114 @@ const stockProductos = [
   {
     id: 1,
     nombre: "Crash Bandicoot",
-    cantidad: 1,
+    stock: 3,
     desc: "Juego plataformero, niveles dificiles",
+    descripcion: "Juego plataformero, niveles dificiles",
     precio: 1200,
     descuento: 100,
     img: "assets/img/Crash.jpg",
+    cantidad:1
+    
+    
   },
   {
     id: 2,
     nombre: "Mortal Kombat X",
-    cantidad: 1,
+    stock: 1,
     desc: "Luchas con los mejores graficos",
+    descripcion:"Luchas con los mejores graficos",
     precio: 1500,
     descuento: 500,
     img: "assets/img/mortal.jpg",
+    cantidad:1
   },
   {
     id: 3,
     nombre: "Pac Man",
-    cantidad: 1,
+    stock: 1,
     desc: "Juego plataformero, niveles basicos",
+    descripcion:"Juego plataformero, niveles basicos",
     precio: 1570,
     descuento: 20,
     img: "assets/img/pacman.jpg",
+    cantidad:1
   },
   {
     id: 4,
     nombre: "Dragon Ball Xenoverse",
-    cantidad: 1,
+    stock: 1,
     desc: "Vive la experiencia dragon ball",
+    descripcion:"Vive la experiencia dragon ball",
     precio: 1000,
     descuento: 100,
     img: "assets/img/dragonball.jpg",
+    cantidad:1
   },
   {
     id: 5,
     nombre: "Naruto Ninja Storm 4",
-    cantidad: 1,
+    stock: 1,
     desc: "La historia de Naruto",
+    descripcion:"La historia de Naruto",
     precio: 1200,
     descuento: 100,
     img: "assets/img/naruto.jpg",
+    cantidad:1
   },
   {
     id: 6,
     nombre: "Shingeki Final Attack",
-    cantidad: 1,
+    stock: 1,
     desc: "Eren Jeager vuelve en formato gamer...",
+    descripcion:"Eren Jeager vuelve en formato gamer...",
     precio: 1200,
     descuento: 100,
     img: "assets/img/shingeki.jpg",
+    cantidad:1
   },
   {
     id: 7,
     nombre: "League of Legends",
-    cantidad: 1,
+    stock: 1,
     desc: "No compres esto por tu bien",
+    descripcion:"No compres esto por tu bien",
     precio: 1400,
     descuento: 500,
     img: "assets/img/league.jpg",
+    cantidad:1
   },
   {
     id: 8,
     nombre: "Call Of Duty Warzone",
-    cantidad: 1,
+    stock: 1,
     desc: "Dispara como nunca",
+    descripcion:"Dispara como nunca",
     precio: 1200,
     descuento: 100,
     img: "assets/img/callduty.jpg",
+    cantidad:1
   },
   {
     id: 9,
     nombre: "Fifa 2019",
-    cantidad: 1,
+    stock: 1,
     desc: "Juego de futbol",
+    descripcion:"Juego de futbol",
     precio: 1400,
     descuento: 100,
     img: "assets/img/fifa.jpg",
+    cantidad:1
   },
   {
     id: 10,
     nombre: "Fornite",
-    cantidad: 1,
+    stock: 1,
     desc: "Battle Royale",
+    descripcion:"Battle Royale",
     precio: 1200,
     descuento: 100,
     img: "assets/img/fornite.jpg",
+    cantidad:1
   },
 ];
 
@@ -158,7 +180,7 @@ if (procesarCompra) {
 }
 
 stockProductos.forEach((prod) => {
-  const { id, nombre, precio, desc, img, cantidad, descuento } = prod;
+  const { id, nombre, precio, desc, img, stock, descuento, cantidad} = prod;
   if (contenedor) {
     contenedor.innerHTML += `
     <div class="card mt-3" style="width: 18rem;">
@@ -167,8 +189,10 @@ stockProductos.forEach((prod) => {
       <h5 class="card-title">${nombre}</h5>
       <p class="card-text">Precio: ${precio}</p>
       <p class="card-text">Descripcion: ${desc}</p>
+      <p class="card-text">Cantidad: ${stock}</p>
       <p class="card-text">Cantidad: ${cantidad}</p>
       <p class="card-text">Descuento: ${descuento}</p>
+      
       <button class="btn btn-primary" onclick="agregarProducto(${id})">Comprar Producto</button>
     </div>
   </div>
@@ -336,5 +360,174 @@ function procesarPedido() {
 
  }
 
- 
+
+
+
+function cargarTabla(listaProductos){
+    let cuerpoTabla = document.querySelector(".section_mantenedor_productos tbody");
+    cuerpoTabla.innerHTML = "";
+
+    let acumuladorFilas = "";
+    listaProductos.forEach(producto => {
+        acumuladorFilas += `
+                <tr>
+                    <th scope="row">${producto.id}</th>
+                    <td>${producto.nombre}</td>
+                    <td>${producto.descripcion}</td>
+                    <td>${producto.precio}</td>
+                    <td>${producto.stock}</td>
+                </tr>
+        `
+    });
+    cuerpoTabla.innerHTML = acumuladorFilas;
+
+}
+
+function buscarProducto(id){
+    let producto = new Producto(id);
+    return producto.getProduct();
+}
+
+crud_form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+})
+
+
+//funcion que capture evento del input crud_id
+
+let inputId = document.getElementById("crud_id");
+inputId.addEventListener("change", (event) =>{
+    event.preventDefault();
+    let id =  inputId.value;
+    let producto = buscarProducto(id);
+    if(producto){
+        crud_nombre.value = producto.nombre;
+        crud_descripcion.value = producto.descripcion;
+        crud_precio.value = producto.precio;
+        crud_stock.value = producto.stock;
+    }else{
+        crud_nombre.value = "";
+        crud_descripcion.value = "";
+        crud_precio.value = "";
+        crud_stock.value = "";
+
+    }
+})
+
+
+function main(){
+    let productosStorage = JSON.parse(localStorage.getItem("productos"));
+    if(!productosStorage){
+        productosStorage = stockProductos;
+        localStorage.setItem("productos", JSON.stringify(productosStorage))
+    }
+
+    cargarTabla(productosStorage);
+
+}
+
+main();
+
+let productosStorage = JSON.parse(localStorage.getItem("productos")) || [];
+
+ class Producto{
+    constructor(id, nombre = "", descripcion = "Sin descripción", precio = 999999, stock = 0){
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion= descripcion;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    getProducts(){
+        productosStorage = productosStorage = JSON.parse(localStorage.getItem("productos")) || []
+        return productosStorage;
+    }
+    getProduct(){
+        productosStorage = JSON.parse(localStorage.getItem("productos")) || []
+        return productosStorage.find(producto => producto.id == this.id);
+    }
+    deleteProduct(){
+        productosStorage = JSON.parse(localStorage.getItem("productos")) || []
+        productosStorage = productosStorage.filter(producto => producto.id != this.id)
+        localStorage.setItem("productos", JSON.stringify(productosStorage))
+        return productosStorage;
+    }
+    updateProduct(){
+        productosStorage = JSON.parse(localStorage.getItem("productos")) || []
+        let producto = productosStorage.find(producto => producto.id ==this.id)
+        producto.nombre= this.nombre;
+        producto.descripcion= this.descripcion;
+        producto.precio = this.precio;
+        producto.stock = this.stock;
+        localStorage.setItem("productos", JSON.stringify(productosStorage))
+        return producto;
+    }
+    addProduct(){
+        productosStorage = JSON.parse(localStorage.getItem("productos")) || []
+        productosStorage.push(
+            {
+                id: this.id,
+                nombre: this.nombre,
+                descripcion: this.descripcion,
+                precio: this.precio, 
+                stock: this.stock
+            }
+            )
+            localStorage.setItem("productos", JSON.stringify(productosStorage))
+        return productosStorage
+    }
+
+}
+
+let btnmodificar = document.getElementById("btn-modificar");
+btnmodificar.addEventListener ("click",(event)=> {
+  event.preventDefault();
+  let id=crud_id.value
+  let nombre=crud_nombre.value
+  let descripcion=crud_descripcion.value
+  let precio=crud_precio.value
+  let stock=crud_stock.value
+  let producto = new Producto (id, nombre, descripcion, precio, stock);
+
+  if (producto.getProduct()){ 
+    producto.updateProduct()
+    cargarTabla(producto.getProducts()) 
+  }
+})
+
+
+let btnagregar = document.getElementById("btn-agregar");
+btnagregar.addEventListener ("click",(event)=> {
+  event.preventDefault();
+  let id=crud_id.value || "";
+  let nombre=crud_nombre.value || "";
+  let descripcion=crud_descripcion.value || "";
+  let precio=crud_precio.value || "";
+  let stock=crud_stock.value || "";
+  let producto = new Producto (id, nombre, descripcion, precio, stock);
+
+  if (producto.getProduct()){ 
+    producto.addProduct()
+    cargarTabla(producto.getProducts()) 
+  }
+})
+
+
+let btneliminar = document.getElementById("btn-eliminar");
+btneliminar.addEventListener ("click",(event)=> {
+  event.preventDefault();
+  let id=crud_id.value
+  let nombre=crud_nombre.value
+  let descripcion=crud_descripcion.value
+  let precio=crud_precio.value
+  let stock=crud_stock.value
+  let producto = new Producto (id, nombre, descripcion, precio, stock);
+
+  if (producto.getProduct()){ 
+    producto.deleteProduct()
+    cargarTabla(producto.getProducts()) 
+  }
+})
+
 
